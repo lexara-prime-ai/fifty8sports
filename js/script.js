e46892982f49119ee1c709808513a1235b1239a1
irfanghat <irfanghat@gmail.com>
2023-07-11 16:26:41 +0300

    UPDATE: UI Update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,7 @@
 function openSideMenu() {
     document.getElementById('side-menu').style.width = '250px';
-
-    // document.getElementById('main').style.marginLeft = '250px';
 }
 
 function closeSideMenu() {
     document.getElementById('side-menu').style.width = '0';
-
-    // document.getElementById('main').style.marginLeft = '0';
-}
+} 
